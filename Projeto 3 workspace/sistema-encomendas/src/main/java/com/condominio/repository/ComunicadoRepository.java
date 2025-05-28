b9ad3d4f0aa56dd52e9acb60c6941bf215c0a0f3
Caio Felipe da Silva Rodrigues <cfsr@cesar.school>
2025-05-28 11:45:57 -0300

    .

--- Projeto 3 workspace/sistema-encomendas/src/main/java/com/condominio/repository/ComunicadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.condominio.repository;
-
-import com.condominio.model.Comunicado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ComunicadoRepository extends JpaRepository<Comunicado, Long> {
-    List<Comunicado> findAllByOrderByDataPublicacaoDesc();
-}
